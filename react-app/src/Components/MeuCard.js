69e2ab2e013e613de55baa2f3f9b9377f6cd6b5b
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-14 21:31:02 -0300

    Cards em defaultContent

---
+++ react-app/src/Components/MeuCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card,  CardContent, Typography} from '@material-ui/core';
+
+export default function MeuCard(props){
+    return(
+        <Card>
+          
+            <CardContent>
+                Testando
+            </CardContent>
+        </Card>
+    )
+}
